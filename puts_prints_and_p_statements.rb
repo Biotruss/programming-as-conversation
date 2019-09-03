71714fa29a890b764274dcaaf04a1cf243ed418c
Craig Messi <53850819+Biotruss@users.noreply.github.com>
2019-09-02 17:56:56 -0700

    Create puts_prints_and_p_statements.rb

---
+++ puts_prints_and_p_statements.rb
@@ -0,0 +1,11 @@
+#Enter examples in irb to see return values!!
+
+#puts prints a string to the screen and creates a "newline" because it adds an invisible newline(\n) character at the end of the string.
+puts "hi"
+
+#print is the same as puts, but does not have the invisible newline(\n) character at the end.
+print "hi"
+
+#p is also the same as puts, but it allows a return value. Which is useful beacause as you know statements return a nil value otherwise. It is also designed for debugging as it shows a more "raw" version of an object.
+p "Hello World"
+ => "Hello World"

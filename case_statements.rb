8d922b322676b9491e74178c4857de1877b136ee
Craig Messi <53850819+Biotruss@users.noreply.github.com>
2019-09-02 18:38:12 -0700

    Create case_statements.rb

---
+++ case_statements.rb
@@ -0,0 +1,33 @@
+#Literally justanother way to write selection(if...elsif...else...end) statements. Use only when there are multiple elsifs in the program. Another thing to note is that not all programming languages have this, but ruby does so here it is.
+name = "Alice"
+ 
+if name == "Alice"
+  puts "Hello, Alice!"
+elsif name == "The White Rabbit"
+  puts "Don't be late, White Rabbit"
+elsif name == "The Mad Hatter"
+  puts "Welcome to the tea party, Mad Hatter"
+elsif name == "The Queen of Hearts"
+  puts "Please don't chop off my head!"
+else
+  puts "Whoooo are you?"
+end 
+
+
+
+#Here is how the same code would look like as a case statement.
+case name 
+ 
+  when "Alice" # translated: when name == "Alice"
+    puts "Hello, Alice!"
+  when "The White Rabbit"
+    puts "Don't be late, White Rabbit"
+  when "The Mad Hatter"
+    puts "Welcome to the tea party, Mad Hatter"
+  when "The Queen of Hearts"
+    puts "Please don't chop off my head!"
+  else 
+    puts "Whoooo are you?"
+end
+
+#Run the program you should get the same result 2 times!!
